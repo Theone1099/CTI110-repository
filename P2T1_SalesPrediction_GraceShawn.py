8f743ea4d264b0b28e7922bd5352846c158c8744
Theone1099 <67303912+Theone1099@users.noreply.github.com>
2020-06-23 00:10:01 -0400

    Add files via upload

---
+++ P2T1_SalesPrediction_GraceShawn.py
@@ -0,0 +1,15 @@
+# This program is based on sales predictions.
+# June 22, 2020
+# CTI-110 P2T1 - Sales Prediction
+# Shawn Grace
+# Get the projected totalsales.
+
+total_sales = float(input('Enter the projected sales:  '))
+
+# Calculate the profit as 23 percent of total sales.
+
+profit = total_sales * 0.23
+
+# Display the profit.
+
+print('The profit is $', format(profit, ',.2f')) 
